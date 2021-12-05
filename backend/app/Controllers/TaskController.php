8e8581e9eb33f11e6531ab041cddda941f917c5b
melaniefauchon <fauchonmelanie@gmail.com>
2021-12-05 22:44:12 +0100

    WIP - Implementation of a method to delete a task according its id - CORS issue

--- backend/app/Controllers/TaskController.php
+++ backend/app/Controllers/TaskController.php
@@ -63,6 +63,9 @@ class TaskController extends CoreController
             http_response_code(200);
             echo json_encode("Tâche supprimée");
         } else {
+            header("Access-Control-Allow-Origin: http://localhost");
+            header("Content-Type: application/json");
+
             http_response_code(404);
             echo json_encode("Tâche non trouvée, veuillez saisir un id correct");
         }
